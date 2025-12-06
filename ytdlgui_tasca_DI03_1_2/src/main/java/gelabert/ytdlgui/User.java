e31e3b659ea6f5881cb4d6481d48f8b718be8907
Jordi Gelabert <jordi.gelabert@gygroup.com>
2025-12-06 20:26:41 +0100

    Entrega 3.1.2 - projecte ytdlgui - 08/12/25

---
+++ ytdlgui_tasca_DI03_1_2/src/main/java/gelabert/ytdlgui/User.java
@@ -0,0 +1,16 @@
+package gelabert.ytdlgui;
+
+public class User {
+    private String name;
+
+    public User() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
