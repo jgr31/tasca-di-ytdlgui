837d49e535c68ed57123cf078af8560ce3f658a3
Jordi Gelabert <jordi.gelabert@gygroup.com>
2025-12-06 20:25:59 +0100

    Entrega 3.1.1 - projecte ytdlgui - 24/11/25

---
+++ ytdlgui_tasca_DI03_1_1/src/main/java/gelabert/ytdlgui/Ytdlgui.java
@@ -0,0 +1,11 @@
+package gelabert.ytdlgui;
+
+public class Ytdlgui {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
+}
+
