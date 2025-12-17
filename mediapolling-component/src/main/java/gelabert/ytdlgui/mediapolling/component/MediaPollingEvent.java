f5d78453882f2c4951ccf71e7bb4754f2ef72ce1
Jordi Gelabert <jordi.gelabert@gygroup.com>
2025-12-17 13:11:31 +0100

    Entrega 3.3 – projecte ytdlgui - 15/12/25 - MediaPollingComponent com a llibreria i integració a ytdlgui

---
+++ mediapolling-component/src/main/java/gelabert/ytdlgui/mediapolling/component/MediaPollingEvent.java
@@ -0,0 +1,25 @@
+package gelabert.ytdlgui.mediapolling.component;
+
+import java.time.OffsetDateTime;
+import java.util.EventObject;
+import java.util.List;
+
+public class MediaPollingEvent extends EventObject {
+
+    private final List<Media> newMedia;
+    private final OffsetDateTime discoveredAt;
+
+    public MediaPollingEvent(Object source, List<Media> newMedia, OffsetDateTime discoveredAt) {
+        super(source);
+        this.newMedia = newMedia;
+        this.discoveredAt = discoveredAt;
+    }
+
+    public List<Media> getNewMedia() {
+        return newMedia;
+    }
+
+    public OffsetDateTime getDiscoveredAt() {
+        return discoveredAt;
+    }
+}
---
+++ mediapolling-component/src/main/java/gelabert/ytdlgui/mediapolling/component/MediaPollingListener.java
@@ -0,0 +1,5 @@
+package gelabert.ytdlgui.mediapolling.component;
+
+public interface MediaPollingListener {
+    void newMediaFound(MediaPollingEvent evt);
+}
